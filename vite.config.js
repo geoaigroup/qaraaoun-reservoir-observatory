87e32d395e22e2868580d07c5848f299a36b6492
Ali Faour <aaf36@mail.aub.edu>
2024-06-21 14:51:53 +0300

    replaced react-scripts with vite

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+
+export default defineConfig({
+    base: '/qaraaoun/',
+    plugins: [react()],
+    server: {    
+        // this ensures that the browser opens upon server start
+        open: true,
+        port: 3000, 
+    },
+})
+
+
