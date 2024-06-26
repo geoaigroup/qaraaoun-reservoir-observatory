569f21a05afd9dada8722d2c445f9ecfc382e87c
Ali Ghandour <ghandour.aub@gmail.com>
2024-06-26 10:07:12 +0000

    debug vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 
 export default defineConfig({
-    base: '/qaraaoun/',
+    base: '/qaraaoun',
     plugins: [react()],
     server: {    
         // this ensures that the browser opens upon server start
@@ -11,5 +11,4 @@ export default defineConfig({
         port: 3000, 
     },
 })
-
 
