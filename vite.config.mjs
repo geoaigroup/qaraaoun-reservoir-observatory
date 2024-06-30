664bc7d44322de9a5771a716a53499fb075da6cb
Ali Faour <aaf36@mail.aub.edu>
2024-06-30 21:55:12 +0300

    fixing problem with static files not loaded

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,35 +1,35 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import {visualizer} from 'rollup-plugin-visualizer';
-
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-    base: '/qaraaoun',
-    plugins: [react()],
-    server: {    
-        // this ensures that the browser opens upon server start
-        open: true,
-        port: 3000, 
-    },
-    build: {
-      rollupOptions: {
-        plugins: [
-          visualizer({
-            filename: './dist/report.html',
-          })
-        ],
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-            if (id.includes('src/components')) {
-              return 'components';
-            }
+  base: process.env.VITE_APP_BASENAME || '/qaraaoun',
+  plugins: [react()],
+  server: {    
+    open: true,
+    port: 3000, 
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        visualizer({
+          filename: './dist/report.html',
+        })
+      ],
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+          if (id.includes('src/components')) {
+            return 'components';
           }
         }
-      },
-      chunkSizeWarningLimit: 3000, // Adjust the limit as needed
-    }
-})
+      }
+    },
+    chunkSizeWarningLimit: 3000,
+  }
+});
+
+
 
