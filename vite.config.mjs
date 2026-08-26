1da9b490e966ca664439c48f5d40f9db3564a63b
Ali J. Ghandour <ghandour.aub@gmail.com>
2026-08-26 12:01:05 +0300

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -7,7 +7,8 @@ export default defineConfig({
   plugins: [react()],
   server: {    
     open: true,
-    port: 3000, 
+    port: 3000,
+	allowedHosts: ['geoai.cnrs.edu.lb'],	
   },
   build: {
     rollupOptions: {
